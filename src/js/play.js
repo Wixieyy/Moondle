970f81d4b57b9bb030288f5b6797d1b78e892d6b
Wixieyy <shendrickwilliams@gmail.com>
2024-05-05 15:07:42 +0200

    input boxes

--- src/js/play.js
+++ src/js/play.js
@@ -6,14 +6,24 @@ const uranus = ["Titania","Oberon","Umbriel","Ariel","Miranda","Puck"]
 const neptune = ["Triton","Proteus","Nereid","Larissa","Galatea","Despina"]
 const pluto = ["Charon","Hydra","Nix"]
 const planets = [earth, mars, jupiter, saturn, uranus, neptune, pluto];
+const boxes = document.querySelectorAll('.box');
 
 start();
 
 function start() {
     const selectedPlanet = planets[(Math.ceil(Math.random() * planets.length)) - 1];
     const selectedMoon = selectedPlanet[(Math.ceil(Math.random() * selectedPlanet.length)) - 1]
-}
 
-function darkMode() {
+    for (let x = 0; x < selectedMoon.length * 6; x++) {
+        boxes[x].style.display = 'block'
+    }
 
+    window.addEventListener('keydown', function(event) {
+        for (const box of boxes) {
+            if (box.textContent === '') {
+                box.textContent = event.key;
+                break;
+            }
+        }
+    });
 }
